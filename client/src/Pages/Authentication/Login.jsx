20ad6780b25b60ba72766c9dd5142baad2491e1f
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-01-19 19:55:57 +0530

    adding : login changes in mac push to code base

--- client/src/Pages/Authentication/Login.jsx
+++ client/src/Pages/Authentication/Login.jsx
@@ -1,86 +1,168 @@
-import { Box, Button, Flex, Input, Text } from '@chakra-ui/react'
-import axios from 'axios';
-import React from 'react'
-import styles from "../../Styles/Store.module.css"
+import { Box, Button, Flex, Input, Text } from "@chakra-ui/react";
+import axios from "axios";
+import React from "react";
+import styles from "../../Styles/Store.module.css";
 import { AiFillLock, AiOutlineGoogle, AiOutlineUser } from "react-icons/ai";
-import { useToast } from '@chakra-ui/react';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useToast } from "@chakra-ui/react";
+import { useState } from "react";
+import { useEffect } from "react";
 const Login = () => {
-   const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const[dataemail, setDataemail] = useState("");
-    const[datapass, setPass] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [dataemail, setDataemail] = useState("");
+  const [datapass, setPass] = useState("");
 
-    const toast = useToast();
+  const toast = useToast();
 
-    const fetchData = async () => {
-      try {
-          const res = await axios.get('https://mock-server-686g.onrender.com/blueRegister');
-         console.log(res.data);
-         res.data.forEach(el => {
-          setDataemail(el.email)
-          setPass(el.password)
-         });
-         
-      } catch (err) {
-          console.error(err.response);
-      }
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(
+        "https://mock-server-686g.onrender.com/blueRegister"
+      );
+      console.log(res.data);
+      res.data.forEach((el) => {
+        setDataemail(el.email);
+        setPass(el.password);
+      });
+    } catch (err) {
+      console.error(err.response);
+    }
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[])
-const handleLogin =()=>{
-  if(email===dataemail && password===datapass){
-    toast({
-      title: 'Login Successful',
-      description: "User found in our database",
-      status: 'success',
-      duration: 9000,
-      isClosable: true,
-    })
-    window.location.replace("/");
-  }else{
-    toast({
-      title: 'Invalid User!',
-      description: "User not found in our database",
-      status: 'error',
-      duration: 3000,
-      isClosable: true,
-    })
-  }
-}
+  }, []);
+  const handleLogin = () => {
+    if (email === dataemail && password === datapass) {
+      toast({
+        title: "Login Successfull",
+        description: "User found in our database",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      window.location.replace("/");
+    } else if (email === "admin@blueapple.com" && password === "arpit@123") {
+      toast({
+        title: "Login as an admin",
+        description: "valid admin ",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      window.location.replace("/");
+    } else {
+      toast({
+        title: "Invalid User!",
+        description: "User not found in our database",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
   return (
-   <>
-   <Box className={styles.log_main} py={5}>
-    <Flex borderRadius="10px" w="70%" m={"auto"}>
-      <Box w={"45%"} minH="600" bgImage="https://cdn.wallpapersafari.com/95/10/Z7FCgJ.png" bgSize={"cover"} bgRepeat="no-repeat" bgPosition={"center"} zIndex="1">
-        <Text fontWeight={"bold"} fontSize={35} mt="20px" className={styles.text_log}>Welcome To Blue Apple</Text>
+    <>
+      <Box className={styles.log_main} py={5}>
+        <Flex borderRadius="10px" w="70%" m={"auto"}>
+          <Box
+            w={"45%"}
+            minH="600"
+            bgImage="https://cdn.wallpapersafari.com/95/10/Z7FCgJ.png"
+            bgSize={"cover"}
+            bgRepeat="no-repeat"
+            bgPosition={"center"}
+            zIndex="1"
+          >
+            <Text
+              fontWeight={"bold"}
+              fontSize={35}
+              mt="20px"
+              className={styles.text_log}
+            >
+              Welcome To Blue Apple
+            </Text>
+          </Box>
+          <Box
+            w={"55%"}
+            minH="600"
+            borderRadius="10px"
+            className={styles.gyay_teal}
+          >
+            <Box mt="20%" w={"80%"} ml="10%">
+              <Text
+                fontSize={40}
+                textAlign="left"
+                className={styles.text_login}
+                fontWeight="bold"
+                color={"white"}
+              >
+                Login Here !
+              </Text>
+              <Box>
+                <Flex alignItems={"center"} gap="2%" pl={5} py={5}>
+                  <AiOutlineUser size={40} color="white" />
+                  <Input
+                    w={"80%"}
+                    borderRadius={20}
+                    border="2px solid white"
+                    textAlign={"center"}
+                    fontSize={18}
+                    fontWeight="400"
+                    type="email"
+                    placeholder="Email / BlueApple ID"
+                    color={"whiteAlpha.600"}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Flex>
+                <Flex alignItems={"center"} gap="2%" pl={5} py={5}>
+                  <AiFillLock size={40} color="white" />
+                  <Input
+                    w={"80%"}
+                    borderRadius={20}
+                    border="2px solid white"
+                    textAlign={"center"}
+                    fontSize={18}
+                    fontWeight="400"
+                    type="password"
+                    placeholder="Password / BlueApple Password"
+                    color={"whiteAlpha.600"}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Flex>
+                <Button
+                  bg={"black"}
+                  color="white"
+                  _hover={{ color: "black", bg: "white" }}
+                  w={"40%"}
+                  border="2px solid white"
+                  fontSize={25}
+                  fontWeight="400"
+                  onClick={handleLogin}
+                >
+                  Login
+                </Button>
+                <br />
+                <Button
+                  bg={"black"}
+                  color="white"
+                  _hover={{ color: "black", bg: "white" }}
+                  w={"80%"}
+                  border="2px solid white"
+                  fontSize={25}
+                  fontWeight="400"
+                  mt={5}
+                >
+                  <AiOutlineGoogle size={25} color="#f9ae72" /> continue with
+                  Google
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </Flex>
       </Box>
-      <Box w={"55%"} minH="600" borderRadius="10px"  className={styles.gyay_teal} >
-        <Box mt="20%" w={"80%"} ml="10%">
-        <Text fontSize={40} textAlign="left" className={styles.text_login} fontWeight="bold" color={"white"}>Login Here !</Text>
-        <Box>
-          <Flex alignItems={"center"} gap="2%" pl={5} py={5}>
-            <AiOutlineUser size={40} color="#f9ae72"/>
-            <Input w={"80%"} borderRadius={20} border="4px solid #f9ae72" textAlign={"center"} fontSize={20} fontWeight="bold" type="email" placeholder='Email / BlueApple ID' onChange={((e)=>setEmail(e.target.value))}/>
-          </Flex>
-          <Flex alignItems={"center"} gap="2%" pl={5} py={5}>
-            <AiFillLock size={40} color="#f9ae72"/>
-            <Input w={"80%"} borderRadius={20} border="4px solid #f9ae72" textAlign={"center"} fontSize={20} fontWeight="bold" type="password" placeholder='Password / BlueApple Password' onChange={((e)=>setPassword(e.target.value))}/>
-          </Flex>
-          <Button bg={"black"} color="teal" w={"40%"} border="2px solid #f9ae72" fontSize={25} fontWeight="bold" onClick={handleLogin}>Login</Button><br/>
-          <Button bg={"black"} color="teal" w={"80%"} border="2px solid #f9ae72" fontSize={25} fontWeight="bold" mt={5}><AiOutlineGoogle size={25} color="#f9ae72" /> Continue with Google</Button>
-        </Box>
-        
-        </Box>
+    </>
+  );
+};
 
-      </Box>
-    </Flex>
-   </Box>
-   </>
-  )
-}
-
-export default Login
+export default Login;
--- client/src/Pages/Mac.jsx
+++ client/src/Pages/Mac.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
 import styles from "../Styles/Mac.module.css";
 import img from"../assets/mac/macMini.png";
+import { Link } from 'react-router-dom';
 const Mac = () => {
   return (
     <>
 <div className={styles.tp}>
-    <div><a href="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/macbookair_light__f0o95rwae8ae_large.svg" alt="product img" /><p>
-MacBook Air</p></a></div>
-    <div><a href="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/macbook_pro_light__e791sjqzt32a_large.svg" alt="product img" /><p>MacBook Pro</p></a></div>
-    <div><a href="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/imac_24_light__colyztscbeeu_large.svg" alt="product img" /><p>iMac 24”</p></a></div>
-    <div><a href="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_mini_light__frtahmzmd4mm_large.svg" alt="product img" /><p>Mac mini
-</p></a></div>
-    <div><a href="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_studio_light__ea3pb1auizu6_large.svg" alt="product img" /><p>Mac Studio</p></a></div>
-    <div><a href="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_pro_light__cj4dvg7thx5y_large.svg" alt="product img" /><p>Mac Pro
-</p></a></div>
-    <div><a href="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_compare_light__emml1umdv9m6_large.svg" alt="product img" /><p>Compare
-</p></a></div>
-    <div><a href="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/displays_light__ewxqx5obdxci_large.svg" alt="product img" /><p>Displays</p></a></div>
-    <div><a href="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_accessories_light__cuds10wyptyu_large.svg" alt="product img" /><p>Accessories</p></a></div>
-    <div><a href="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_os_light__d0zczt34scq6_large.svg" alt="product img" /><p>Ventura</p></a></div>
-    <div><a href="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_shop_light__layuggud1xe2_large.svg" alt="product img" /><p>Shop mac</p></a></div>
+    <div><Link to="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/macbookair_light__f0o95rwae8ae_large.svg" alt="product img" /><p>
+MacBook Air</p></Link></div>
+    <div><Link to="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/macbook_pro_light__e791sjqzt32a_large.svg" alt="product img" /><p>MacBook Pro</p></Link></div>
+    <div><Link to="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/imac_24_light__colyztscbeeu_large.svg" alt="product img" /><p>iMac 24”</p></Link></div>
+    <div><Link to="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_mini_light__frtahmzmd4mm_large.svg" alt="product img" /><p>Mac mini
+</p></Link></div>
+    <div><Link to="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_studio_light__ea3pb1auizu6_large.svg" alt="product img" /><p>Mac Studio</p></Link></div>
+    <div><Link to="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_pro_light__cj4dvg7thx5y_large.svg" alt="product img" /><p>Mac Pro
+</p></Link></div>
+    <div><Link to="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_compare_light__emml1umdv9m6_large.svg" alt="product img" /><p>Compare
+</p></Link></div>
+    <div><Link to="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/displays_light__ewxqx5obdxci_large.svg" alt="product img" /><p>Displays</p></Link></div>
+    <div><Link to="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_accessories_light__cuds10wyptyu_large.svg" alt="product img" /><p>Accessories</p></Link></div>
+    <div><Link to="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_os_light__d0zczt34scq6_large.svg" alt="product img" /><p>Ventura</p></Link></div>
+    <div><Link to="#"><img src="https://www.apple.com/v/mac/home/br/images/familybrowser/mac_shop_light__layuggud1xe2_large.svg" alt="product img" /><p>Shop mac</p></Link></div>
 </div>
 
 <div className={styles.offer}>
   <p>Get 3% Daily Cash back with Apple Card. And pay for your new Mac over 12 months, interest‑free when you choose to check out with Apple Card Monthly Installments.*</p>
-  <a href="#" title='offer'>Learn more {">"}</a>
+  <Link to="#" title='offer'>Learn more {">"}</Link>
 </div>
 {/* new product components */}
 <div className={styles.mac}>
@@ -34,7 +35,7 @@ MacBook Air</p></a></div>
     <h3>Don’t take it lightly.</h3>
     <p>From $1199</p>
     <button style={{width:"80px", height:"auto", padding:"5px 0px", backgroundColor:"#1577ED", borderRadius:"20px", color:"white", margin:"20px 0px"}}>Buy</button>
-    <a href="#" className={styles.learn}>Learn more {">"}</a>
+    <Link to="#" className={styles.learn}>Learn more {">"}</Link>
   </div>
   <div className={styles.macImg}>
    <center> <img src="https://www.apple.com/v/mac/home/br/images/overview/hero/macbook_air_m2__r1jrymq4ftea_large.jpg" alt="macbook" style={{margin:"20px 0px"}}/></center>
@@ -49,7 +50,7 @@ MacBook Air</p></a></div>
     <h3 style={{color:"white"}}>Pro anywhere.</h3>
     <p style={{color:"white"}}>From $1299</p>
     <button style={{width:"80px", height:"auto", padding:"5px 0px", backgroundColor:"#1577ED", borderRadius:"20px", color:"white", margin:"20px 0px"}}>Buy</button>
-    <a href="#" className={styles.learn}>Learn more {">"}</a>
+    <Link to="#" className={styles.learn}>Learn more {">"}</Link>
   </div>
   <div className={styles.macImg}>
    <center> <img src="https://www.apple.com/v/mac/home/br/images/overview/hero/macbook_pro_13__e3r46kd69eoi_large.jpg" alt="macbook" style={{margin:"20px 0px"}}/></center>
@@ -64,7 +65,7 @@ MacBook Air</p></a></div>
     <h3 style={{color:"white"}}>Mover. Maker. Boundary breaker.</h3>
     <p style={{color:"white"}}>From $1999</p>
     <button style={{width:"80px", height:"auto", padding:"5px 0px", backgroundColor:"#1577ED", borderRadius:"20px", color:"white", margin:"20px 0px"}}>Buy</button>
-    <a href="#" className={styles.learn}>Learn more {">"}</a>
+    <Link to="#" className={styles.learn}>Learn more {">"}</Link>
   </div>
   <div className={styles.macImg}>
    <center> <img src="https://www.apple.com/v/mac/home/bs/images/overview/hero/macbook_pro_14_16__dmqm5vr9l7yq_large.jpg" alt="macbook" style={{margin:"20px 0px"}}/></center>
@@ -80,7 +81,7 @@ MacBook Air</p></a></div>
     <h3 style={{color:"white"}}>More muscle. More hustle.</h3>
     <p style={{color:"white"}}>From $599</p>
     <button style={{width:"80px", height:"auto", padding:"5px 0px", backgroundColor:"#1577ED", borderRadius:"20px", color:"white", margin:"20px 0px"}}>Buy</button>
-    <a href="#" className={styles.learn}>Learn more {">"}</a>
+    <Link to="#" className={styles.learn}>Learn more {">"}</Link>
   </div>
   <div className={styles.macImg}>
    <center> <img src={img} alt="macbook" style={{margin:"10px 0px"}}/></center>
@@ -95,20 +96,20 @@ MacBook Air</p></a></div>
 A gift for every wish.</h2>
   <p style={{color:"white", fontSize:"25px", fontWeight:"400"}}>Find something for everyone.</p>
   <p style={{color:"white", fontSize:"25px", fontWeight:"400"}}>this Lunar New Year.</p>
-  <a href="https://www.apple.com/us/shop/goto/gifts/chinese_new_year" className={styles.learn} style={{color:"white"}}>Shop the gift guide{">"}</a>
+  <Link to="https://www.apple.com/us/shop/goto/gifts/chinese_new_year" className={styles.learn} style={{color:"white"}}>Shop the gift guide{">"}</Link>
 </div>
 <br />
 <div className={styles.banner}>
   <h2 style={{color:"white"}}>Apple at Work</h2>
   <p style={{color:"white", fontSize:"25px", fontWeight:"400"}}>Get the power to take your business to the next level.</p>
-  <a href="https://www.apple.com/business/" className={styles.learn} style={{color:"white"}}>Learn about Apple at Work {">"}</a>
+  <Link to="https://www.apple.com/business/" className={styles.learn} style={{color:"white"}}>Learn about Apple at Work {">"}</Link>
 </div>
 <br />
 
 <div className={styles.banner2}>
   <h2 style={{color:"white"}}>Apple and Education</h2>
   <p style={{color:"white", fontSize:"25px", fontWeight:"400"}}>Empowering educators and students to move the world forward.</p>
-  <a href="https://www.apple.com/business/" className={styles.learn} style={{color:"white"}}>Learn about Apple and Education{">"}</a>
+  <Link to="https://www.apple.com/business/" className={styles.learn} style={{color:"white"}}>Learn about Apple and Education{">"}</Link>
 </div>
 
     </>
